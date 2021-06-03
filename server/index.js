07305633a7a8ce8f9f50851f60a3bf2419451d6f
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-06-03 21:50:17 +0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => console.log("Connected to MongoDB")
+);
+
+// PUBLIC FOLDER
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// MULTER
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// ROUTES
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+
+// API
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+
+app.listen(5000, () => console.log("Backend server is running"));
